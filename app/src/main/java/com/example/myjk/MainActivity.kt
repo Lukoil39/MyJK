a4854a12d10804420654f3f5aee3db09b8e41ba0
Lukoil39 <goshavasilevskiy@mail.ru>
2020-04-03 18:35:38 +0200

    Second commit

--- app/src/main/java/com/example/myjk/MainActivity.kt
+++ app/src/main/java/com/example/myjk/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.myjk
 import androidx.appcompat.app.AppCompatActivity
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +12,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun toastMe(view : View) {
-        val myToast = Toast.makeText(this, "Hello creator!", Toast.LENGTH_SHORT)
+    fun toastMe() {
+        val myToast = Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT)
         myToast.show()
 
     }
